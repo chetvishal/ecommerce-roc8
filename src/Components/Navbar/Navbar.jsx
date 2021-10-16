e4c0f06ae9e557f4a0c52b9e4a111f21887f14ed
chetvishal <vishal1982c@gmail.com>
2021-10-16 10:17:57 +0530

    feat: added navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+
+export const NavigationBar = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#" style={{marginLeft: "1rem"}}>Navbar scroll</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="mr-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link href="#action1">Home</Nav.Link>
+                    <Nav.Link href="#action2">Link</Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="mr-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
---
+++ src/Pages/Products/Products.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const Products = () => {
+
+    return (
+        <>
+            <h1>Products Page</h1>
+        </>
+    )
+
+}
